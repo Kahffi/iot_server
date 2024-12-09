9774768a982f360995b71a11318076d4d5672ddf
Kahffi <daffaalkahffi24@gmail.com>
2024-12-10 00:43:01 +0700

    initial commit

--- api/index.py
+++ api/index.py
@@ -1,5 +1,9 @@
 from flask import Flask
+import pickle
+import numpy as np 
 
+
+model = pickle.load(open("../knn_model.pkl", "rb"))
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +13,12 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/predict')# gets the values that were sent to '/predict' by 'index.html'
+def predict():
+    final_features = [np.array([1.0,0.02394, 523.54])]# turns the form values into a Numpy array
+    prediction = model.predict(final_features)# makes a prediction using the values in the created Numpy array
+
+    output = prediction[0]# gets the prediction as a string
+
+    return output# displays the prediction inside the '<b>{{ prediction_text }}</b>' that we've seen in 'index.html'
